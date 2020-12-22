67054aa1bd6bde011316aa75345ef14d30d6c469
Emilien Devos <contact@emiliendevos.be>
2020-12-22 23:32:57 +0100

    fix bash to ash and use canister

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 JAVA_FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions
 -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M
 -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15
