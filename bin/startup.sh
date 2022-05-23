c97cd353051c29cb51608c6c379c289740872b68
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2022-05-23 16:09:01 -0700

    Enable stats return and disable autocycle and autorestart on private servers
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- bin/startup.sh
+++ bin/startup.sh
@@ -6,12 +6,16 @@ if [ "$CHART_NAME" == "ranked" ]; then
     # temporary hack due to configmap being read only, need to be fixed
     cp plugins/Bolty/config-readonly.yml plugins/Bolty/config.yml
     sed -i "s/%SERVER-NAME%/$SERVER_NAME/g" plugins/Bolty/config.yml
+    echo "[INFO] Ranked server detected... updating PGM config."
+    sed -i 's/%START-TIME%/"30s"/g' plugins/PGM/config.yml
+    sed -i "s/%MATCH-LIMIT%/6/g" plugins/PGM/config.yml
+    sed -i "s/%STATS-RETURN%/-1/g" plugins/PGM/config.yml
 fi
 
 if [ -n "$MAX_PLAYERS" ]; then
-		echo "[INFO] Changing the maximum of players."
-		sed -i '/max-players/d' /minecraft/server.properties
-		echo "max-players=${MAX_PLAYERS}" >> /minecraft/server.properties
+        echo "[INFO] Changing the maximum of players."
+        sed -i '/max-players/d' /minecraft/server.properties
+        echo "max-players=${MAX_PLAYERS}" >> /minecraft/server.properties
 fi
 
 if [ -n "$OPERATORS" ]; then
@@ -29,18 +33,20 @@ if [ -n "$OPERATORS" ]; then
 fi
 
 if [ "$CHART_NAME" != "ranked" ]; then
-    echo "[INFO] Not a ranked server detected... removing Ingame, Events, AutoKiller, Bolty, Matrix and Idly plugins."
+    echo "[INFO] Not a ranked server detected... removing Ingame, Events, AutoKiller, Bolty, Matrix and Idly plugins and updating PGM config."
     rm -f /minecraft/plugins/ingame.jar /minecraft/plugins/Events.jar \
         /minecraft/plugins/AutoKiller-*.jar /minecraft/plugins/Bolty-*.jar \
         /minecraft/plugins/Matrix-*.jar \
         /minecraft/plugins/Idly.jar
-
+    sed -i "s/%START-TIME%/-1/g" plugins/PGM/config.yml
+    sed -i "s/%MATCH-LIMIT%/-1/g" plugins/PGM/config.yml
+    sed -i 's/%STATS-RETURN%/"3s"/g' plugins/PGM/config.yml
 fi
 
 if [ "$CHART_NAME" = "privateserver" ]; then
     echo "[INFO] Private server detected... activating the whitelist."
     sed -i '/white-list/d' /minecraft/server.properties
-	echo "white-list=true" >> /minecraft/server.properties
+    echo "white-list=true" >> /minecraft/server.properties
 fi
 
 if [ "$NODE_NAME" != "ns522982" ]; then
